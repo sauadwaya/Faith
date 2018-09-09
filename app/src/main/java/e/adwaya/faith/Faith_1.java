ddc8ff44d9a65e69b248f27815709f581aefeec0
sauadwaya <adwaya.97@gmail.com>
2018-09-09 16:59:17 -0400

    First commit

---
+++ app/src/main/java/e/adwaya/faith/Faith_1.java
@@ -0,0 +1,16 @@
+package e.adwaya.faith;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Faith_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faith_1);
+
+
+        //Put content here
+    }
+}
