03567d382a66b2d65132ca5ca4e8825f699348af
sauadwaya <adwaya.97@gmail.com>
2018-09-09 17:16:56 -0400

    Second Commit Adwaya

---
+++ app/src/main/java/e/adwaya/faith/acceptTerms.java
@@ -0,0 +1,13 @@
+package e.adwaya.faith;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class acceptTerms extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accept_terms);
+    }
+}
